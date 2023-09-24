8187218767d8845f4773e7a4151de8d2d4284be1
Anna Golmakova <ann.golmakova@gmail.com>
2023-09-24 17:50:28 +0500

    Попробуем локалхост

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const NotFoundError = require('./errors/not-found-err');
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
