903f3c5ee674f74c684d8e52ed68aa9896b7a450
Anna Golmakova <ann.golmakova@gmail.com>
2023-10-01 22:54:15 +0500

    Поправила cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,7 +8,12 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports.cors = (req, res, next) => {
   const { method } = req;
+  const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
@@ -16,11 +21,5 @@ module.exports.cors = (req, res, next) => {
     return res.end();
   }
 
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
+  return next();
 };
